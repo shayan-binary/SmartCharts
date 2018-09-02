5b1a2148184a6d18a480cc1781ba6182d10af3e2
shayan <shayanypn@shayans-MacBook-Pro.local>
2018-09-02 06:31:18 +0430

    Updates

--- pwa-checklist/dist/sw.js
+++ pwa-checklist/dist/sw.js
@@ -1,20 +1,20 @@
 var cacheName = 'SmartCharts';
 var filesToCache = [
-  './',
-  './index.html',
-  './dist/smartcharts.css',
-  './dist/babel-polyfill.min.js',
-  './dist/chartiq.min.js',
-  './dist/react.js',
-  './dist/react-dom.js',
-  './dist/react-transition-group.js',
-  './dist/mobx.js',
-  './dist/mobx-react.js',
-  './dist/smartcharts.js'
+  '/',
+  '/index.html',
+  '/dist/smartcharts.css',
+  '/dist/babel-polyfill.min.js',
+  '/dist/chartiq.min.js',
+  '/dist/react.js',
+  '/dist/react-dom.js',
+  '/dist/react-transition-group.js',
+  '/dist/mobx.js',
+  '/dist/mobx-react.js',
+  '/dist/smartcharts.js'
 ];
 
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+  console.log('[ServiceWorker] Install', e);
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
