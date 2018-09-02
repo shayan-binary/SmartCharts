c2d766dc110c27702eb45adcb8bf9e282b52cc89
shayan <shayanypn@shayans-MacBook-Pro.local>
2018-09-02 20:08:56 +0430

    Updates

--- pwa-checklist/sw.js
+++ pwa-checklist/sw.js
@@ -1,39 +1,40 @@
-// var cacheName = 'SmartCharts';
-// var filesToCache = [
-//   './index.html',
-//   './dist/smartcharts.css',
-//   './dist/babel-polyfill.min.js',
-//   './dist/chartiq.min.js',
-//   './dist/react.js',
-//   './dist/react-dom.js',
-//   './dist/react-transition-group.js',
-//   './dist/mobx.js',
-//   './dist/mobx-react.js',
-//   './dist/smartcharts.js'
-// ];
+var cacheName = 'SmartCharts';
+var pathname = window.location.pathname;
+var filesToCache = [
+  '/index.html',
+  '/dist/smartcharts.css',
+  '/dist/babel-polyfill.min.js',
+  '/dist/chartiq.min.js',
+  '/dist/react.js',
+  '/dist/react-dom.js',
+  '/dist/react-transition-group.js',
+  '/dist/mobx.js',
+  '/dist/mobx-react.js',
+  '/dist/smartcharts.js'
+];
 
-// self.addEventListener('install', function(e) {
-//   console.log(filesToCache);
-//   console.log(e.currentTarget.filesToCache);
-//   console.log('[ServiceWorker] Install', e);
-//   e.waitUntil(
-//     caches.open(cacheName).then(function(cache) {
-//       console.log('[ServiceWorker] Caching app shell');
-//       return cache.addAll(filesToCache);
-//     })
-//   );
-// });
+self.addEventListener('install', function(e) {
+  console.log(filesToCache);
+  console.log(e.currentTarget.filesToCache);
+  console.log('[ServiceWorker] Install', e);
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
 
-// self.addEventListener('activate', function(event){
-//   console.log('activate', event);
-//   event.waitUntil(self.clients.claim());
-// });
+self.addEventListener('activate', function(event){
+  console.log('activate', event);
+  event.waitUntil(self.clients.claim());
+});
 
-// self.addEventListener('fetch', function(event){
-//   console.log('fetch');
-//   event.respondWith(
-//     caches.match(event.request, {ignoreSearch:true}).then(function(response){
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
+self.addEventListener('fetch', function(event){
+  console.log('fetch');
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(function(response){
+      return response || fetch(event.request);
+    })
+  );
+});
