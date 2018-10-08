738293ec5a631618768e042431638d075f5d9396
shayan <shayanypn@shayans-MacBook-Pro.local>
2018-10-08 07:27:43 +0330

    Updates

--- pwa-caching/sw.js
+++ pwa-caching/sw.js
@@ -14,9 +14,11 @@ var filesToCache = [
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName)
+    .then(function(cache) {
       return cache.addAll(filesToCache);
     })
+    .catch( error =>  console.log('install error', error) )
   );
 });
 
@@ -27,8 +29,36 @@ self.addEventListener('activate', function(event){
 self.addEventListener('fetch', function(event){
   if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {return;}
   event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(function(response){
+    caches.match(event.request, {ignoreSearch:true})
+    .then(function(response){
       return response || fetch(event.request);
     })
+    .catch( error =>  console.log('fetch error', error) )
   );
 });
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response;
+//         } else {
+//           return fetch(event.request)
+//             .then(function(res) {
+//               return caches.open(CACHE_DYNAMIC_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request.url, res.clone());
+//                   return res;
+//                 })
+//             })
+//             .catch(function(err) {       // fallback mechanism
+//               return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+//                 .then(function(cache) {
+//                   return cache.match('/offline.html');
+//                 });
+//             });
+//         }
+//       })
+//   );
+// });  
